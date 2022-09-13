dfd09a8a17435d5b23d8e8e69eb4b5c182856d5a
Tatiana Kalinina <tankakalinka@gmail.com>
2022-09-13 22:43:01 +0300

    add ex 33 Find N In Numbers

---
+++ ex_33/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая определяет, 
+// присутствует ли заданное число в массиве.
+
+Console.Write("Input number N: ");
+int N = int.Parse(Console.ReadLine()!);
+
+int[] numbers = CreateArray(10);
+PrintArray(numbers);
+
+bool isNumberInArray = NumberInArray(numbers);
+string txt = isNumberInArray == true? $"{N} IS in numbers" : $"{N} IS NOT in numbers";
+Console.WriteLine(txt);
+
+int[] CreateArray(int size)
+{
+    int[] numbers = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        numbers[i] = new Random().Next(-100, 101);
+    }    
+    return numbers;
+}
+
+
+void PrintArray(int[] numbers)
+{
+    foreach (int number in numbers)
+    {
+        Console.Write($"{number} ");
+    }
+    Console.WriteLine();
+}
+
+bool NumberInArray(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] != N)
+                return false;
+        }
+    return true;
+}
