a48be427d0be3570dc8a37e2db922841c53530d9
Tatiana Kalinina <tankakalinka@gmail.com>
2022-09-13 22:48:40 +0300

    add ex 37 Two Elements Produce

---
+++ ex_37/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 37: Найдите произведение пар чисел в одномерном массиве. 
+// Парой считаем первый и последний элемент, второй и предпоследний и 
+// т.д. Результат запишите в новом массиве.
+
+int[] numbers = CreateArray(9);
+PrintArray(numbers);
+int[] multipliedVector = NumbersProduceFromPair(numbers);
+PrintArray(multipliedVector);
+
+int[] CreateArray(int size)
+{
+    int[] numbers = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        numbers[i] = new Random().Next(-100, 101);
+    }    
+    return numbers;
+}
+
+
+void PrintArray(int[] numbers)
+{
+    foreach (int number in numbers)
+    {
+        Console.Write($"{number} ");
+    }
+    Console.WriteLine();
+}
+
+
+int[] NumbersProduceFromPair(int[] numbers)
+
+{
+    int[] multipliedVector = new int[(numbers.Length) / 2];
+    for (int i = 0; i < numbers.Length/2; i++)
+        multipliedVector[i] = numbers[i] * numbers[numbers.Length - 1 - i];
+    return multipliedVector;
+}
